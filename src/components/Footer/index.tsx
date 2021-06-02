ee83f5dc15dd554daa80eb99fe554a1cce9e8254
rogerrm95 <rogerrm@live.com>
2021-06-02 18:53:30 -0300

    Update: Cores no componente Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ export function Footer() {
         <Flex bg='gray.100' width='100%' p='4' align='center' justify='space-between' boxShadow='dark-lg'>
             <Image src='/logo.svg' alt='logo' width={100}/>
             <Text display='flex' flexDirection='row'>Producted with ❤️ by  
-                <Text color='yellow.500' fontWeight='bold' ml='2'>Roger Marques</Text></Text>
+                <Text color='gray.600' fontWeight='bold' ml='2'>Roger Marques</Text></Text>
             <Text fontWeight='bold' fontSize='lg'>
                 2021
             </Text>
